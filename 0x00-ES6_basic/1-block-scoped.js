b4e5934b41db5d5cdd5fc1d56ebc36257cec7c9a
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2024-10-04 14:45:17 +0000

    done

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -1,10 +1,11 @@
-export default function taskBlock(trueOrFalse) {
-	const task = false;
-	const task2 = true;
-	
-	if (trueOrFalse) {
-		const task = true;
-		const task2 = false;
-	}
-	return [task, task2];
+export default function taskBlock(isTrue) {
+  const task = false;
+  let task2 = true;
+
+  if (isTrue) {
+    const task = true;
+    let task2 = false;
+  }
+
+  return [task, task2];
 }
