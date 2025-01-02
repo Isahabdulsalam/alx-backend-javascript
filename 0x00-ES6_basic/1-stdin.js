0480546a5bb74a78b9c608a96ba2e8b573cebb56
Isah Abdulsalam <Isahabdulsalam416@gmail.com>
2025-01-02 03:05:41 +0100

    done

--- 0x00-ES6_basic/1-stdin.js
+++ 0x00-ES6_basic/1-stdin.js
@@ -1,16 +1,12 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
-
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+process.stdin.setEncoding('utf8');
+console.log("Welcome to ALX, what is your name?");
+process.stdin.on('readable', () => {
+  const name = process.stdin.read();
+  if (name !== null) {
+    console.log(`Your name is: ${name.trim()}`);
+    process.stdin.end();
+  }
+});
+process.stdin.on('end', () => {
+  console.log("This important software is now closing");
+});
