6b8de14fcd0333cf6f73785b5fd14fbd6369446e
Isah Abdulsalam <isahabdulsalam416@gmail.com>
2025-01-31 15:01:34 +0000

    done

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,16 +1,11 @@
-process.stdout.write('Welcome to Holberton School, what is your name?\n');
+process.stdout.write("Welcome to ALX, what is your name?\n");
 
-if (process.stdin.isTTY) {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
+process.stdin.on("data", (data) => {
+    const name = data.toString().trim();
+    console.log(`Your name is: ${name}`);
     process.exit();
-  });
-} else {
-  process.stdin.on('data', (data) => {
-    process.stdout.write(`Your name is: ${data.toString()}`);
-    process.exit();
-  });
-  process.on('exit', () => {
-    process.stdout.write('This important software is now closing\n');
-  });
-}
+});
+
+process.on("exit", () => {
+    console.log("This important software is now closing");
+});
